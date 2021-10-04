583ed7ad9bb7d6ae078c94f9c14db1d4dfdbe2ee
oleksandrtaran <ujujzk@gmail.com>
2021-10-04 23:16:26 +0000

    edge-to-egde

--- presentation/src/main/java/com/ujujzk/ee/presentation/MainActivity.kt
+++ presentation/src/main/java/com/ujujzk/ee/presentation/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.ujujzk.ee.presentation
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.ujujzk.ee.presentation.di.KOIN_NAV_APP_CICERONE
 import com.ujujzk.ee.presentation.navigation.BackButtonListener
 import com.ujujzk.ee.presentation.navigation.ScreenFactory
+import com.ujujzk.ee.presentation.tools.setNavigationBarDarkIcons
+import com.ujujzk.ee.presentation.tools.setStatusBarDarkIcons
 import org.koin.android.ext.android.inject
 import org.koin.core.qualifier.named
 
 
-class MainActivity: AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val cicerone : Cicerone<Router> by inject(named(KOIN_NAV_APP_CICERONE))
+    private val cicerone: Cicerone<Router> by inject(named(KOIN_NAV_APP_CICERONE))
     private val appNavigator by lazy { AppNavigator(this, R.id.container, supportFragmentManager) }
     private val screens: ScreenFactory by inject()
 
@@ -30,17 +35,13 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
         setupEdgeToEdge()
     }
 
-    private fun setupEdgeToEdge(){
-        window.apply {
-            decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    private fun setupEdgeToEdge() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                decorView.systemUiVisibility = decorView.systemUiVisibility or
-                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR or
-                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            }
+        window.apply {
+
+            setStatusBarDarkIcons(true)
+            setNavigationBarDarkIcons(true)
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 statusBarColor = ContextCompat.getColor(context, R.color.colorStatusBar)
@@ -60,7 +61,7 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
     }
 
     override fun onBackPressed() {
-        supportFragmentManager.fragments.filter{ it.isVisible }.forEach {
+        supportFragmentManager.fragments.filter { it.isVisible }.forEach {
             if (!(it as BackButtonListener).onBackPressed())
                 finish()
         }
--- presentation/src/main/java/com/ujujzk/ee/presentation/tools/Extensions.kt
+++ presentation/src/main/java/com/ujujzk/ee/presentation/tools/Extensions.kt
@@ -1,9 +1,9 @@
 package com.ujujzk.ee.presentation.tools
 
+import android.graphics.Color
 import android.graphics.Rect
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.os.Build
+import android.view.*
 import androidx.annotation.LayoutRes
 import androidx.core.view.*
 
@@ -21,5 +21,46 @@ inline fun <reified T : ViewGroup.LayoutParams> View.postUpdateLayoutParams(cros
         val params = layoutParams as T
         block(params)
         layoutParams = params
+    }
+}
+
+
+@Suppress("DEPRECATION")
+fun Window.setStatusBarDarkIcons(dark: Boolean) {
+    when {
+        Build.VERSION_CODES.R <= Build.VERSION.SDK_INT -> insetsController?.setSystemBarsAppearance(
+            if (dark) WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS else 0,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+        Build.VERSION_CODES.M <= Build.VERSION.SDK_INT -> decorView.systemUiVisibility = if (dark) {
+            decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        }
+        else -> if (dark) {
+            // dark status bar icons not supported on API level below 23, set status bar
+            // color to black to keep icons visible
+            statusBarColor = Color.BLACK
+        }
+    }
+}
+
+@Suppress("DEPRECATION")
+fun Window.setNavigationBarDarkIcons(dark: Boolean) {
+    when {
+        Build.VERSION_CODES.R <= Build.VERSION.SDK_INT -> insetsController?.setSystemBarsAppearance(
+            if (dark) WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS else 0,
+            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+        )
+        Build.VERSION_CODES.O <= Build.VERSION.SDK_INT -> decorView.systemUiVisibility = if (dark) {
+            decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        } else {
+            decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+        }
+        else -> if (dark) {
+            // dark navigation bar icons not supported on API level below 26, set navigation bar
+            // color to black to keep icons visible
+            navigationBarColor = Color.BLACK
+        }
     }
 }
